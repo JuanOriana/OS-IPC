adbc9b5ec839e4313a3e35d870b68c62ea2c7f57
Juan Oriana <joriana@itba.edu.ar>
2021-03-17 18:41:41 -0300

    arg reading

---
+++ app.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    DIR *pDir = opendir(argv[1]);
+    if (pDir == NULL)
+    {
+        fprintf(stderr, "Can not open directory %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    closedir(pDir);
+    return 0;
+}
