bf883c64cb1aabda8e6e08fa46443839f7fc75c0
Juan Oriana <joriana@itba.edu.ar>
2021-03-20 17:14:08 -0300

    piping and forking

--- app.c
+++ app.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define CHILD_COUNT 3
+#define READ_END 0
+#define WRITE_END 1
 
 int main(int argc, char const *argv[])
 {
@@ -8,15 +14,41 @@ int main(int argc, char const *argv[])
         fprintf(stderr, "Usage: %s <files>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-    //
 
-    // DIR *pDir = opendir(argv[1]);
-    // if (pDir == NULL)
-    // {
-    //     fprintf(stderr, "Can not open directory %s\n", argv[1]);
-    //     exit(EXIT_FAILURE);
-    // }
+    int fileCount = argc - 1;
+    pid_t childIDs[CHILD_COUNT];
+    int pipes[CHILD_COUNT][2];
 
-    // closedir(pDir);
+    for (int i = 0; i < CHILD_COUNT; i++)
+    {
+        if (pipe(pipes[i]) < 0)
+        {
+            perror("Unsuccesful pipe");
+            abort();
+        }
+    }
+
+    for (i = 0; i < CHILD_COUNT; i++)
+    {
+        if ((childIDs[i] = fork()) < 0)
+        {
+            perror("Unsuccesful fork");
+            abort();
+        }
+        else if (childIDs[i] == 0)
+        {
+            execv("./slave", NULL);
+        }
+    }
+
+    for (i = 0; i < CHILD_COUNT; i++)
+    {
+        if (waitpid(childIDs[i], NULL, 0) < 0)
+        {
+            perror("Unsuccesful wait");
+            abort();
+        }
+    }
+
     return 0;
 }
