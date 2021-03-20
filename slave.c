4a2a440389f3ce8a81fc32f241a3824bc1d81dd7
Brittany <brlin@itba.edu.ar>
2021-03-20 17:59:58 -0300

    slave 2.0

--- slave.c
+++ slave.c
@@ -1,21 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int readFile(char *line);
 
 int main(int argc, char const *argv[]) {
     if (argc <= 1) {
-        printf("Error cantidad de parametros.\n");
+        printf("Not enough parameters.\n");
         return -1;
     }
     for (int i = 1; i < argc; i++) {
         FILE *fd;
-        char c[256];
         if ((fd = fopen(argv[i], "r")) == NULL) {
             perror("fopen");
-            abort();
+            exit(-1);
         }
-        fscanf(fd, "%[^\n]", c);
-        printf("Data from file %s: %s\n", argv[i], c);
+        char line[256];
+        fscanf(fd, "%[^\n]", line);
+        printf("%d\n", readFile(line));
         fclose(fd);
     }
     return 0;
 }
+
+int readFile(char *line) {
+    char *token = strtok(line, " ");
+    int count = 0;
+    int suma = 0;
+    while (token != NULL) {
+        int aux = atoi(token);
+        count++;
+        if ((aux == 0 && strcmp(token, "0") != 0)|| count >= 3) {
+            printf("Error in parameters\n");
+            exit(-1);
+        }
+        suma += aux;
+        token = strtok(NULL, " ");
+    }
+    return suma;
+}
+
