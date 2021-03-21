5ae970087efe22233baf86306f2d8ec1c39121cb
Juan Oriana <joriana@itba.edu.ar>
2021-03-21 20:27:38 -0300

    Styling slave

--- slave.c
+++ slave.c
@@ -7,17 +7,18 @@ int readFile(char *line);
 int main(int argc, char const *argv[])
 {
     char str[256];
+    char line[256];
+    FILE *fd;
     while (scanf("%s", str) != EOF)
     {
         if (strcmp(str, "") == 0)
             return 0;
-        FILE *fd;
+
         if ((fd = fopen(str, "r")) == NULL)
         {
             perror("fopen");
             exit(-1);
         }
-        char line[256];
         fscanf(fd, "%[^\n]", line);
         printf("%d\n", readFile(line));
         fclose(fd);
@@ -31,9 +32,10 @@ int readFile(char *line)
     char *token = strtok(line, " ");
     int count = 0;
     int sum = 0;
+    int aux;
     while (token != NULL)
     {
-        int aux = atoi(token);
+        aux = atoi(token);
         count++;
         if ((aux == 0 && strcmp(token, "0") != 0) || count >= 3)
         {
