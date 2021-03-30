492bd8a2c2b9b9b737cb5156faf0d24b98f01992
Brittany <brlin@itba.edu.ar>
2021-03-30 09:08:02 -0300

    slave cnf error

--- slave.c
+++ slave.c
@@ -7,9 +7,6 @@
 #include <unistd.h>
 #define BUFF_SIZE 1024
 #define MAX_FILE_LENGTH 4096
-#define NEGATIVE_ERROR(error, function) if(error == -1) {perror(function);  exit(EXIT_FAILURE);}
-#define NULL_ERROR(error, function) if(error == NULL) {perror(function);  exit(EXIT_FAILURE);}
-#define EOF_ERROR(error, function) if(error == EOF) {perror(function);  exit(EXIT_FAILURE);}
 
 void solve(char *file);
 
@@ -41,23 +38,37 @@ void solve(char *file) {
     int retValue = -1;
 
     retValue = sprintf(command,"minisat %s | grep -o -e \"Number of.*[0-9]\\+\" -e \"CPU time.*\" -e \".*SATISFIABLE\" | tr \"\\n\" \"\t\" | tr -d \" \t\"", file);
-    NEGATIVE_ERROR(retValue, "sprintf");
+    if (retValue == -1) {
+        perror("sprintf");
+        exit(EXIT_FAILURE);
+    }
 
     FILE *stream = popen(command, "r");
-    NULL_ERROR(stream, "popen");
+    if (stream == NULL) {
+        perror("popen");
+        exit(EXIT_FAILURE);
+    } 
 
-    char *p = fgets(buff, MAX_FILE_LENGTH, stream);
-    NULL_ERROR(p, "fgets");
+    if (fgets(buff, MAX_FILE_LENGTH, stream) == NULL) {
+        perror("fgets");
+        exit(EXIT_FAILURE);
+    }
 
     retValue = pclose(stream);
-    NEGATIVE_ERROR(retValue, "pclose");
+    if (retValue == -1) {
+        perror("pclose");
+        exit(EXIT_FAILURE);
+    }
 
     int variables, clauses = -1;
     float cpuTime = -1;
     char state[14] = "";
 
     retValue = sscanf(buff, "Numberofvariables:%dNumberofclauses:%dCPUtime:%fs%s", &variables, &clauses, &cpuTime, state);
-    EOF_ERROR(retValue, "sscanf");
+    if (retValue == EOF) {
+        perror("sscanf");
+        exit(EXIT_FAILURE);
+    }
 
     printf("PID:%d\nFilename:%s\nNumberofvariables:%d Numberofclauses:%d CPUtime:%f %s\n", getpid(), basename(file), variables,
                 clauses, cpuTime, state);
