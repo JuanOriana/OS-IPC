6934e275c19376f2eefd0f267271013c060e2b1f
Brittany <brlin@itba.edu.ar>
2021-03-30 08:44:51 -0300

    slave cnf

--- .bash_history
+++ .bash_history
@@ -1,2 +1,94 @@
 docker exec -ti [docker-id] bash
 exit
+cd root
+ls
+gcc -Wall -o slave slave.c
+gcc -Wall -std=c99 -o slave slave.c
+./slave
+./slave arch.txt
+gcc -Wall -std=c99 -o slave slave.c
+valgrind ./slave
+gcc -Wall -std=c99 -o slave slave.c
+gcc -Wall -std=c99 -o slave slave.c
+gcc -Wall -std=c99 -o slave slave.c
+gcc -Wall -std=c99 -o slave slave.c
+gcc -Wall -std=c99 -o slave slave.c
+./slave arch.txt
+./slave arch.txt
+gcc -Wall -std=c99 -o slave slave.c
+./slave arch.txt
+gcc -Wall -std=c99 -o slave slave.c
+./slave arch.txt
+gcc -Wall -std=c99 -o slave slave.c
+./slave arch.txt
+gcc -Wall -std=c99 -o slave slave.c
+gcc -Wall -std=c99 -o slave slave.c
+./slave arch.txt
+gcc -Wall -std=c99 -o slave slave.c
+gcc -Wall -std=c99 -o slave slave.c
+gcc -Wall -std=c99 -o slave slave.c
+./slave arch.txt
+gcc -Wall -std=c99 -o slave slave.c
+gcc -Wall -std=c99 -o slave slave.c
+./slave arch.txt
+valgrind ./slave
+valgrind ./slave
+./slave arch.txt
+gcc -Wall -std=c99 -o slave slave.c
+./slave arch.txt
+gcc -Wall -std=c99 -o slave slave.c
+./slave arch.txt
+gcc -Wall -std=c99 -o slave slave.c
+./slave arch.txt
+gcc -Wall -std=c99 -o slave slave.c
+./slave arch.txt
+gcc -Wall -std=c99 -o slave slave.c
+gcc -Wall -std=c99 -o slave slave.c
+./slave arch.txt
+gcc -Wall -std=c99 -o slave slave.c
+./slave arch.txt
+gcc -Wall -std=c99 -o slave slave.c
+./slave arch.txt
+gcc -Wall -std=c99 -o slave slave.c
+./slave
+gcc -Wall -std=c99 -o slave slave.c
+./slave
+gcc -Wall -std=c99 -o slave slave.c
+./slave
+gcc -Wall -std=c99 -o slave slave.c
+./slave
+gcc -Wall -std=c99 -o slave slave.c
+./slave
+gcc -Wall -std=c99 -o slave slave.c
+./slave
+gcc -Wall -std=c99 -o slave slave.c
+./slave
+gcc -Wall -std=c99 -o slave slave.c
+./slave
+./slave
+./slave
+./slave
+gcc -Wall -std=c99 -o slave slave.c
+./slave
+./slave
+gcc -Wall -std=c99 -o slave slave.c
+./slave
+valgrind ./slave
+exit
+exit
+makefile
+make
+pvs-studio-analyzer trace -- make
+clear
+make all
+cd ./.vscode-server
+exit
+make all
+make
+make appmake
+appmake
+clear
+cd root
+cd root
+minisat
+exit
--- slave.c
+++ slave.c
@@ -3,57 +3,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <libgen.h>
+#include <unistd.h>
 #define BUFF_SIZE 1024
+#define MAX_FILE_LENGTH 4096
+#define NEGATIVE_ERROR(error, function) if(error == -1) {perror(function);  exit(EXIT_FAILURE);}
+#define NULL_ERROR(error, function) if(error == NULL) {perror(function);  exit(EXIT_FAILURE);}
+#define EOF_ERROR(error, function) if(error == EOF) {perror(function);  exit(EXIT_FAILURE);}
 
-int readFile(char *line);
+void solve(char *file);
 
 int main(int argc, char const *argv[])
 {
     setvbuf(stdout,NULL,_IONBF,0);
     char *path = NULL;
-    char line[256];
     size_t len = 0;
     ssize_t lineSize = 0;
-    FILE* fd;
 
     while ((lineSize = getline(&path, &len, stdin)) > 0)
     {
         if(strcmp(path,"") == 0)
             return 0;
 
-        path[strcspn(path, "\n")] = 0; //Clearing path
+        path[strcspn(path, "\n")] = 0; //Clearing path ; path = nombre del archivo
 
-        if ((fd = fopen(path, "r")) == NULL)
-        {
-            perror("fopen");
-            exit(-1);
-        }
-
-        fscanf(fd, "%[^\n]", line);
-        printf("%d\n", readFile(line));
-        fclose(fd);
+        solve(path);
 
     }
     free(path);
     return 0;
 }
 
-int readFile(char *line)
-{
-    char *token = strtok(line, " ");
-    int count = 0;
-    int sum = 0;
-    while (token != NULL)
-    {
-        int aux = atoi(token);
-        count++;
-        if ((aux == 0 && strcmp(token, "0") != 0) || count >= 3)
-        {
-            printf("Error in parameters\n");
-            exit(-1);
-        }
-        sum += aux;
-        token = strtok(NULL, " ");
-    }
-    return sum;
+
+void solve(char *file) {
+    char command[MAX_FILE_LENGTH] = "";
+    char buff[MAX_FILE_LENGTH] = "";
+    int retValue = -1;
+
+    retValue = sprintf(command,"minisat %s | grep -o -e \"Number of.*[0-9]\\+\" -e \"CPU time.*\" -e \".*SATISFIABLE\" | tr \"\\n\" \"\t\" | tr -d \" \t\"", file);
+    NEGATIVE_ERROR(retValue, "sprintf");
+
+    FILE *stream = popen(command, "r");
+    NULL_ERROR(stream, "popen");
+
+    char *p = fgets(buff, MAX_FILE_LENGTH, stream);
+    NULL_ERROR(p, "fgets");
+
+    retValue = pclose(stream);
+    NEGATIVE_ERROR(retValue, "pclose");
+
+    int variables, clauses = -1;
+    float cpuTime = -1;
+    char state[14] = "";
+
+    retValue = sscanf(buff, "Numberofvariables:%dNumberofclauses:%dCPUtime:%fs%s", &variables, &clauses, &cpuTime, state);
+    EOF_ERROR(retValue, "sscanf");
+
+    printf("PID:%d\nFilename:%s\nNumberofvariables:%d Numberofclauses:%d CPUtime:%f %s\n", getpid(), basename(file), variables,
+                clauses, cpuTime, state);
+
 }
